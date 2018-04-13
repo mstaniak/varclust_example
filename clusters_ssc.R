b42e388d25850d6d0f75cebcb65222fe2d7b45e7
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2018-04-13 23:58:08 +0200

    ssc vs varclust, minor fixes

--- clusters_ssc.R
+++ clusters_ssc.R
@@ -1,3 +1,5 @@
+# Data analysis using varclust initialized with SSC clusterings.
+## Load libraries, functions, SSC clusterings and datasets ----
 library(varclust)
 library(dplyr)
 library(tidyr)
@@ -5,22 +7,24 @@ library(readr)
 library(stringr)
 library(ggplot2)
 library(ggmap)
-load("data/march_less.rda")
-load("data/march_daily.rda")
+library(readr)
 source("functions.R")
 source("ssc_clusters_vecs.R")
-
+load("./data/march_less.rda")
+load("./data/march_daily.rda")
+load("./data/stations.rda")
+sensor_locations <- read_csv("./data/sensor_locations.csv")
+## Run varclust on each of the SSC clusterings ----
 every_cl <- list(rep(1, 263), c(1, cl2[2:263]), cl3, cl4, cl5, cl6, cl7, cl8, cl9,
                  cl10, cl11, cl12, cl13, cl14, cl15, cl16, cl17, cl18, cl19, cl20)
 every_vcl <- lapply(1:20, function(x)
                mlcc.reps(march_less, numb.clusters = x, numb.runs = 30, max.iter = 50,
-                         initial.segmentations = every_cl[[x]]))
+                         initial.segmentations = list(every_cl[[x]][1:263])))
 lapply(every_vcl, function(x) x$BIC)
-# The biggest BIC: 19 clusters
+### The largest BIC: 19 clusters
 clusters_ssc19 <- print.clusters.vec(march_less, cl19)
-clusters_vcl9 <- print.clusters(march_less, vclust19)
-sum(clusters_ssc19 == clusters_vcl9) # żadnych zmian
-# A jak dla innych?
+clusters_vcl9 <- print.clusters(march_less, every_vcl[[19]])
+### What about other partitions?
 lapply(1:20, function(x) {
   cl_ssc <- print.clusters.vec(march_less, (every_cl[[x]])[1:263])
   cl_vcl <- print.clusters(march_less, every_vcl[[x]])
@@ -30,15 +34,13 @@ lapply(1:20, function(x) {
     "Diff dim"
   }
 })
-# Tylko tutaj zaszły zmiany
-cl_ssc2 <- print.clusters.vec(march_less, (every_cl[[2]])[1:263])
-cl_vcl2 <- print.clusters(march_less, every_vcl[[2]])
-# draw.map(every_cl[[2]]][1:263])
-cl_ssc3 <- print.clusters.vec(march_less, (every_cl[[3]])[1:263])
-cl_vcl3 <- print.clusters(march_less, every_vcl[[3]])
-
-draw.map(vclust8$segmentation, clusters = c(2, 4, 6, 7, 8))
-print.clusters(march_less, vclust8)
-
-print.clusters(march_less, vclust10)
-draw.map(vclust10$segmentation, clusters = c(1:2, 5:7, 9:10))
+## Draw maps showing the clusterings ----
+print.clusters(march_less, every_vcl[[19]])
+### First for pressure
+draw.map(every_vcl[[19]]$segmentation, clusters = c(5, 10, 17, 18))
+### Then for particular matter
+draw.map(every_vcl[[19]]$segmentation, clusters = c(1:3, 6:8, 12:16, 19))
+### Compare to SSC initial clustering
+print.clusters.vec(march_less, cl19)
+draw.map(cl19, clusters = c(5, 10, 17, 18))
+draw.map(cl19, clusters = c(1:3, 6:8, 12:16, 19))
